f85e38f6a364e66f8a74114c409566c79d17fae1
KarolinaTad <k.t.1993@bk.ru>
2023-12-15 00:46:27 +0300

    Task 2 from Arrays seminar

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Задайте массив на 10 целых чисел.
+// Напишите программу, которая определяет количество чётных чисел в массиве.
+
+int N = 10;
+int[] a=new int[N];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+a[i]=random.Next(1,101);
+for(int i=0;i<a.Length;i++)
+System.Console.Write($"{a[i], 4}");
+
+int count=0;
+for(int i=0; i < a.Length; i++)
+{
+if(a[i] % 2 == 0)
+count++;
+}
+
+System.Console.WriteLine();
+System.Console.WriteLine(count);
