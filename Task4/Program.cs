62313b37d31bf8f8df4cd5c94330fd1e6817f4ab
KarolinaTad <k.t.1993@bk.ru>
2023-12-15 19:02:34 +0300

    Добавили исключение

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -3,32 +3,39 @@
 // Старший разряд числа должен располагаться на 0-м индексе массива, младший – на последнем. 
 // Размер массива должен быть равен количеству цифр.
 
-    Console.WriteLine("Напишите число от 1 до 100 000: ");
-    int number = Convert.ToInt32(Console.ReadLine());
-    int length = 0;
-    int digit = 1;
-        
-    // Cчитаем колличество цифр
-    for (int i = 0; i < 7; i++)
-    {
-        if (number / digit >= 1)
-            length++;
-        digit *= 10;
-    }
+//Ввод числа
+Console.WriteLine("Напишите число от 1 до 100 000: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number < 1) | (number > 100000))
+{
+    System.Console.WriteLine("Число введено некорректно. ");
+    return;
+}
 
-    //Создаем массив 
-    int[] array = new int[length];
+// Cчитаем колличество цифр
+int length = 0;
+int digit = 1;
+int maxlength = 6;
+for (int i = 0; i <= maxlength; i++)
+{
+    if (number / digit >= 1)
+        length++;
+    digit *= 10;
+}
 
-    //Заполняем массив
-    for (int i = array.Length - 1; i >= 0; i--)
-    {
+//Создаем массив 
+int[] array = new int[length];
 
-        array[i] = number % 10;
-        number /= 10;
-    }
-    // Вывод массива
-    for (int i = 0; i < length; i++)
-        Console.Write($"{array[i],4}");
+//Заполняем массив
+for (int i = array.Length - 1; i >= 0; i--)
+{
+    array[i] = number % 10;
+    number /= 10;
+}
+
+// Вывод массива
+for (int i = 0; i < length; i++)
+    Console.Write($"{array[i],4}");
 
 
 
